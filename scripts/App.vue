41121f4762264770b1d91a76d5a4df9be8ed33f8
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-11-08 21:56:06 +0530

    Making source menu responsive

--- scripts/App.vue
+++ scripts/App.vue
@@ -15,24 +15,36 @@
       </router-link>
     </div>
     <div class="grid grid-sidebar-left">
+      
       <div class="one-half">
-        <nav>
-          <ul class="news-scoure-menu">
-            <li class="menu-header">Communities</li>
-            <li v-for="link in links.communities">
-              <router-link :to="link.to">
-                <svg :class="link.iconClass"><use v-bind:xlink:href=link.iconID></use></svg>
-                {{ link.title }}
-              </router-link>
-            </li>
-            <li class="menu-header">Blogs</li>
-            <li v-for="link in links.blogs">
-              <router-link :to="link.to">
-                {{ link.title }}
-              </router-link>
-            </li>
-          </ul>
-        </nav>
+        <div class="menu-toggle">
+          <input type="checkbox" id=source-toggle>
+          <label class="alignright" for="source-toggle">
+            <span class="css-icon-hamburger">
+              <span>Toggle</span>
+            </span>
+            <span>Sources</span>
+          </label>
+          <div class="menu-toggle-content">
+            <nav>
+              <ul class="news-scoure-menu">
+                <li class="menu-header">Communities</li>
+                <li v-for="link in links.communities">
+                  <router-link :to="link.to">
+                    <svg :class="link.iconClass"><use v-bind:xlink:href=link.iconID></use></svg>
+                    {{ link.title }}
+                  </router-link>
+                </li>
+                <li class="menu-header">Blogs</li>
+                <li v-for="link in links.blogs">
+                  <router-link :to="link.to">
+                    {{ link.title }}
+                  </router-link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
 
       <div class="one-half">
